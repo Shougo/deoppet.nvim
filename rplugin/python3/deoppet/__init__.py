9b85f6080d0a9e4d449acb049ad06e1e4bd77581
Shougo Matsushita <Shougo.Matsu@gmail.com>
2017-07-27 16:36:27 +0900

    Add handler

---
+++ rplugin/python3/deoppet/__init__.py
@@ -0,0 +1,17 @@
+# ============================================================================
+# FILE: __init__.py
+# AUTHOR: Shougo Matsushita <Shougo.Matsu at gmail.com>
+# License: MIT license
+# ============================================================================
+import neovim
+
+
+@neovim.plugin
+class DeoppetHandlers(object):
+
+    def __init__(self, vim):
+        self._vim = vim
+
+    @neovim.function('_deoppet', sync=False)
+    def init_channel(self, args):
+        self._vim.vars['deoppet#_channel_id'] = self._vim.channel_id
