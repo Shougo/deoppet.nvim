3931cfe2b5936ffa1c2b9eccd05b42039e616460
Shougo Matsushita <Shougo.Matsu@gmail.com>
2017-09-16 18:43:57 +0900

    Improve the parser

--- rplugin/python3/deoppet/parser.py
+++ rplugin/python3/deoppet/parser.py
@@ -4,13 +4,68 @@
 # License: MIT license
 # ============================================================================
 
+import re
+
 
 class Parser():
 
     def __init__(self):
+        self.lines = []
+        self.linenr = 0
+        self.line_max = 0
         pass
 
     def parse(self, text):
-        for line in text.splitlines():
-            pass
-        return []
+        self.lines = text.splitlines()
+        self.linenr = 0
+        self.line_max = len(self.lines)
+
+        snippets = []
+        while self.linenr < self.line_max:
+            line = self.lines[self.linenr]
+            if re.search('^\s*#|^\s*$', line):
+                # Skip
+                self.linenr += 1
+                continue
+            if not re.search('^\s*snippet\s+', line):
+                # Error
+                return []
+
+            snippet = self.parse_one_snippet()
+            return [snippet]
+            if not snippet:
+                # Error
+                return []
+            snippets.append(snippet)
+        return snippets
+
+    def parse_one_snippet(self):
+        line = self.lines[self.linenr]
+        snippet = {}
+        m = re.search('^\s*snippet\s+(.*)$', line)
+        if not m:
+            return {}
+        snippet['trigger'] = m.group(1)
+        snippet['text'] = ''
+
+        self.linenr += 1
+        while self.linenr < self.line_max:
+            line = self.lines[self.linenr]
+            if re.search('^\s*(\S+)', line):
+                return self.parse_text(snippet)
+
+            line += 1
+
+        # Error
+        return {}
+
+    def parse_text(self, snippet):
+        while self.linenr < self.line_max:
+            line = self.lines[self.linenr]
+            m = re.search('^\s*(\S+)', line)
+            if not m:
+                return snippet
+
+            snippet['text'] += m.group(1)
+            self.linenr += 1
+        return snippet
--- test/rplugin/python3/deoppet/test_parser.py
+++ test/rplugin/python3/deoppet/test_parser.py
@@ -5,7 +5,14 @@ from deoppet.parser import Parser
 def test_parse():
     parser = Parser()
 
-    test_snippet = """
+    test_snippet0 = """
+"""
+    test_snippet1 = """
+snippet    foo
+   foobar
+"""
+
+    test_snippet2 = """
 snippet    foo
 abbr       bar
 alias      baz
@@ -13,8 +20,16 @@ regexp     '^% '
 options    word
    foobar
 """
-    assert parser.parse(test_snippet) == [{
-        'name': 'foo',
+
+    assert parser.parse(test_snippet0) == []
+
+    assert parser.parse(test_snippet1) == [{
+        'trigger': 'foo',
+        'text': 'foobar',
+    }]
+
+    assert parser.parse(test_snippet2) == [{
+        'trigger': 'foo',
         'abbr': 'bar',
         'alias': ['baz'],
         'regexp': '^% ',
