c180a6a738bbd03eeefc41e20a642f3b629659b2
Shougo Matsushita <Shougo.Matsu@gmail.com>
2017-09-23 14:15:01 +0900

    Implemented jump feature

--- rplugin/python3/deoppet/mapping.py
+++ rplugin/python3/deoppet/mapping.py
@@ -4,13 +4,16 @@
 # License: MIT license
 # ============================================================================
 
-# from deoppet.util import debug
+from deoppet.util import debug
 
 
 class Mapping():
 
     def __init__(self, vim):
         self._vim = vim
+
+    def debug(self, expr):
+        return debug(self._vim, expr)
 
     def mapping(self, name):
         if name == 'expand':
@@ -38,29 +41,43 @@ class Mapping():
         linenr = cursor[0]
         buf = self._vim.current.buffer
         snippet = snippets[trigger]
-        next_text = self._vim.call('deoppet#util#_get_next_text')
         cur_text = cur_text[: len(cur_text) - len(trigger)]
+        next_text = self._vim.call('deoppet#util#_get_next_text')
         # debug(self._vim, cur_text)
         # debug(self._vim, snippet['trigger'])
         # debug(self._vim, next_text)
         buf[linenr-1] = cur_text + snippet['text'] + next_text
         col = self._vim.call('len', cur_text + snippet['text'])
-        ns = self._vim.call('nvim_init_mark_ns', 'deoppet')
+
         ids = []
+        self._ns = self._vim.call('nvim_init_mark_ns', 'deoppet')
         for tabstop in snippet['tabstops']:
             ids.append(self._vim.call('nvim_buf_set_mark',
-                                      buf.number, ns, 0,
+                                      buf.number, self._ns, '',
                                       tabstop['row'] + linenr,
                                       tabstop['col'] + 1))
         bvars['deoppet_marks'] = ids
+        self.cursor(linenr, col, next_text)
+
+    def jump(self, is_forward):
+        bvars = self._vim.current.buffer.vars
+        if 'deoppet_marks' not in bvars or not bvars['deoppet_marks']:
+            return
+        buf = self._vim.current.buffer
+        marks = bvars['deoppet_marks']
+        mark = self._vim.call('nvim_buf_lookup_mark',
+                              buf.number, self._ns, marks[0])
+        # Insert mode offset
+        mark[2] -= 1
+        next_text = buf[mark[1]-1][mark[2]:]
+        self.cursor(mark[1], mark[2], next_text)
+        bvars['deoppet_marks'] = marks[1:] + [marks[0]]
+
+    def cursor(self, linenr, col, next_text):
+        # self.debug(next_text)
         if next_text:
             self._vim.call('cursor', [linenr, col + 1])
             self._vim.command('startinsert')
         else:
             self._vim.call('cursor', [linenr, col])
             self._vim.command('startinsert!')
-
-    def jump(self, is_forward):
-        bvars = self._vim.current.buffer.vars
-        if 'deoppet_marks' not in bvars:
-            return
