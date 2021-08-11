10ad13620e31c01e61c35f849805a6d02d9b0461
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-08-11 20:07:44 +0900

    Add ddc source

--- denops/ddc/sources/deoppet.ts
+++ denops/ddc/sources/deoppet.ts
@@ -2,17 +2,53 @@ import {
   BaseSource,
   Candidate,
   Context,
-  Denops,
+  DdcOptions,
   SourceOptions,
-} from "https://deno.land/x/ddc_vim@v0.0.11/types.ts";
+} from "https://deno.land/x/ddc_vim@v0.0.13/types.ts";
+import {
+  Denops,
+  fn,
+  vars,
+} from "https://deno.land/x/ddc_vim@v0.0.13/deps.ts#^";
 
 export class Source extends BaseSource {
   async gatherCandidates(
     denops: Denops,
-    _context: Context,
-    _options: SourceOptions,
-    _params: Record<string, unknown>,
+    context: Context,
+    _ddcOptions: DdcOptions,
+    _sourceOptions: SourceOptions,
+    _sourceParams: Record<string, unknown>,
+    completeStr: string,
   ): Promise<Candidate[]> {
-    return [];
+    let snippets = await vars.b.get(denops, "deoppet_snippets");
+    if (!snippets) {
+      return [];
+    }
+
+    const wordMatch = /\w+$/.exec(context.input);
+    const charsMatch = /\S+$/.exec(context.input);
+    if (wordMatch && charsMatch && wordMatch[0] != charsMatch[0]) {
+      snippets = snippets.filter((v) => v.options.word);
+    }
+
+    let ret = [];
+    for (const key in snippets) {
+      const val = snippets[key];
+      const menu = val.abbr ? val.abbr : val.text;
+
+      const triggerCandidates = [val.trigger].concat(val.alias ? val.alias : [])
+        .map((v) => ({
+          word: v,
+          menu: menu,
+        }));
+
+      if (
+        (val.options.head && /^\s*\S+$/.test(context.input)) || !val.regexp ||
+        await fn.matchstr(context.input, val.regexp) != ""
+      ) {
+        ret = ret.concat(triggerCandidates);
+      }
+    }
+    return ret;
   }
 }
--- plugin/deoppet.vim
+++ plugin/deoppet.vim
@@ -8,3 +8,11 @@ if exists('g:loaded_deoppet')
   finish
 endif
 let g:loaded_deoppet = 1
+
+let s:name = fnamemodify(expand('<sfile>'), ':t:r')
+
+silent! call ddc#register_source({
+      \ 'name': s:name,
+      \ 'path': printf('%s/denops/ddc/sources/%s.ts',
+      \                fnamemodify(expand('<sfile>'), ':h:h'), s:name),
+      \ })
